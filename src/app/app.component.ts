b5aacca824107656d4f295351ba99f0247b9e3b8
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2021-05-22 19:59:17 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    StorageService,
+  ]
 })
 export class AppComponent {
   title = 'Hello World';
